51e73116dfee014e8bde178cdcf501abb1917b29
walaa wahba <60180186+InfinitySolutionsPs@users.noreply.github.com>
2026-09-23 08:33:27 +0300

    Add complete employee create and edit dialog

---
+++ drizzle/0006_previous_invaders.sql
@@ -0,0 +1,4 @@
+ALTER TABLE `employees` ADD `first_name` text;--> statement-breakpoint
+ALTER TABLE `employees` ADD `father_name` text;--> statement-breakpoint
+ALTER TABLE `employees` ADD `grandfather_name` text;--> statement-breakpoint
+ALTER TABLE `employees` ADD `family_name` text;
